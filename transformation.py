321efd6fd6a3d2be71a269d958a3cd351e499007
MenaAllahMohamed <36733242+MenaAllahMohamed@users.noreply.github.com>
2021-05-28 14:40:40 +0200

    Update transformation.py
    add word2vec

--- transformation.py
+++ transformation.py
@@ -1,5 +1,6 @@
 from sklearn import feature_extraction
-
+import gensim
+from gensim.models import Word2Vec
 
 def bag_of_words(corpus, ngram_range=(1, 2)):
     transformer = feature_extraction.text.CountVectorizer(ngram_range=ngram_range)
@@ -10,4 +11,9 @@ def bag_of_words(corpus, ngram_range=(1, 2)):
 def tf_idf(corpus, ngram_range=(1, 2)):
     transformer = feature_extraction.text.TfidfVectorizer(ngram_range=ngram_range)
     transformer.fit(corpus)
+    return transformer
+
+
+def w2vec(corpus): 
+    transformer = Word2Vec(corpus)
     return transformer
